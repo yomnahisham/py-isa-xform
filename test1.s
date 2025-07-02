6c37e4a1a0c6aa73b6f4c29f545f24fc33f80cd7
jacquesjeanelia <159173424+jacquesjeanelia@users.noreply.github.com>
2025-07-02 19:56:48 +0300

    fixing ecalls in simulator

--- test1.s
+++ test1.s
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # ZX16 Instruction Test Program (Corrected for all immediate constraints)
 # Tests all instructions except B-type (branch) instructions
 
@@ -106,3 +107,21 @@ test_data:
 .byte 0x12, 0x34, 0x56, 0x78
 .ascii "Hello"
 .align 2
+=======
+# --- Print integer 42 ---
+ADDI x6, 42       # x6 (a0) = 0 + 42
+ECALL 1           # ecall 1: print int
+
+# --- Print string "Hello" ---
+AUIPC x6, 0       # x6 = PC (current address)
+ADDI  x6, msg # x6 = x6 + offset to msg
+ECALL 2           # ecall 2: print string
+
+# --- Exit ---
+ECALL 3           # ecall 3: exit
+
+# --- Data section ---
+.data
+msg: .ascii "Hello"
+.byte 0           # zero-terminated
+>>>>>>> 1faeca9 (fixing ecalls in simulator)
