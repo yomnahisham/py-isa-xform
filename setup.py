6bc0f4fde475881c460b25e3aebca53b3a53845e
Yomna Hisham <yomnaothman@aucegypt.edu>
2025-07-04 03:59:08 +0300

    release v1.0.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ except FileNotFoundError:
 
 setup(
     name="py-isa-xform",
-    version="0.1.0",
+    version="1.0.0",
     description="A comprehensive ISA transformation toolkit",
     long_description=long_description,
     long_description_content_type="text/markdown",
